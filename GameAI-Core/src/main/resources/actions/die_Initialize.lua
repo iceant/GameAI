635b70f62a578cfc033a2f937bf88f47b397a2ed
Chen Peng <pizer.chen@gmail.com>
2024-06-29 17:32:25 +0800

    demo blackboard usage

--- GameAI-Core/src/main/resources/actions/die_Initialize.lua
+++ GameAI-Core/src/main/resources/actions/die_Initialize.lua
@@ -2,4 +2,8 @@ print("die_Initialize")
 print("Context", context)
 print("Blackboard", context:getBlackboard())
 --context:getBlackboard():set("Name", "Chen Peng")
-context:getBlackboard():addLuaSource("Name", function() return {1, "Chen Peng"} end, 10000)
+local count = context:getBlackboard():get("Count") or 0
+count = count + 1
+context:getBlackboard():set("Count", count)
+context:getBlackboard():addLuaSource("Name", function() return {1, "I'm "..count} end, 10000)
+

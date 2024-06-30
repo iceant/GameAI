820bdd2c16e9f138e05eb73fb1ddda7ba1c7b032
Chen Peng <pizer.chen@gmail.com>
2024-06-30 13:05:33 +0800

    Shutdown Hook 可以用来处理退出时保存 blackboard

--- GameAI-Core/src/main/java/game/ai/GameAIApplication.java
+++ GameAI-Core/src/main/java/game/ai/GameAIApplication.java
@@ -1,12 +1,28 @@
 package game.ai;
 
 import game.ai.utils.BehaviorTreeReader;
+import game.ai.utils.JsonUtil;
+import lombok.AllArgsConstructor;
 
 import java.io.InputStream;
 
+@AllArgsConstructor
+class ShutdownHook extends Thread{
+    DefaultGameAIContext context;
+
+
+    @Override
+    public void run() {
+        String json = JsonUtil.toJson(context.getBlackboard());
+        System.out.println(json);
+    }
+}
 
 public class GameAIApplication {
     public static void main(String[] args) throws Exception{
+        DefaultGameAIContext context = new DefaultGameAIContext(new Blackboard());
+
+        Runtime.getRuntime().addShutdownHook(new ShutdownHook(context));
         if(args.length<2){
             System.out.println("java game.ai.GameAIApplication <path> <script>");
             return;
@@ -16,7 +32,6 @@ public class GameAIApplication {
         InputStream inputStream = loader.getResourceAsStream(args[1]);
         BehaviorTree tree = BehaviorTreeReader.read(inputStream);
         inputStream.close();
-        DefaultGameAIContext context = new DefaultGameAIContext(new Blackboard());
         while(true){
             tree.update(context);
         }
